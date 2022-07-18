c9134cd523c9278f79d60a8bf0445eca693fc58e
Morgan Baccus <53062082+MorganBaccus@users.noreply.github.com>
2022-07-18 14:18:08 -0700

    Add files via upload

---
+++ scrimba_react_tutorial/Lesson 13/components/List.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function List() {
+    return (
+        <ul>
+            <input type="checkbox" />
+            <p>Make bed</p>
+            <input type="checkbox" />
+            <p>Laundry</p>
+            <input type="checkbox" />
+            <p>Meal prep</p>
+        </ul>
+    )
+}
+
+export default List
