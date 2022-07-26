0e89b79efed8e3a42195c221edf787d83b7ca535
Morgan Baccus <53062082+MorganBaccus@users.noreply.github.com>
2022-07-26 16:05:47 -0700

    Update App.js

--- scrimba_react_tutorial/To Do App/Phase 6/App.js
+++ scrimba_react_tutorial/To Do App/Phase 6/App.js
@@ -10,42 +10,47 @@
  * 3. In the TodoItem component, make it so when the `onChange` event happens, it calls the `handleChange` method and passes the id of the todo into the function
  */
 
- import React from "react"
- import TodoItem from "./TodoItem"
- import todosData from "./todosData"
- 
- class App extends React.Component {
-     constructor() {
-         super()
-         this.state = {
-             todos: todosData
-         }
-         this.handleChange = this.handleChange.bind(this)
-     }
-     
-     handleChange(id) {
-         this.setState(prevState => {
-             const updatedTodos = prevState.todos.map(todo => {
-                 if (todo.id === id) {
-                     todo.completed = !todo.completed
-                 }
-                 return todo
-             })
-             return {
-                 todos: updatedTodos
-             }
-         })
-     }
-     
-     render() {
-         const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-         
-         return (
-             <div className="todo-list">
-                 {todoItems}
-             </div>
-         )    
-     }
- }
- 
- export default App
+import React from "react"
+import TodoItem from "./TodoItem"
+import todosData from "./todosData"
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo
+            })
+            console.log(prevState.todos)
+            console.log(updatedTodos)
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+    
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+        
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )    
+    }
+}
+
+export default App
